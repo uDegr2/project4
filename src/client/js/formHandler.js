6000cb1271f33a1a46f605f8dbe33afd5d6c5393
unknown <uDegr@proton.me>
2023-02-23 18:06:52 +0100

    added handle method for api call on ui

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,21 @@
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    let url = document.getElementById('name').value;
+   
+        const response = await fetch('/apiCall', {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "url" : url })
+    })
+   
+    
+    const data = await response.json();
+    // Client.updateUI(data);
+}
+
+export {handleSubmit}
