d22be1c49c3c56dfecb7018aade1fb9e7e634e6d
unknown <uDegr@proton.me>
2023-02-23 18:06:16 +0100

    added entry js class

---
+++ src/client/index.js
@@ -0,0 +1,15 @@
+import { updateUI } from './js/uiHandler'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+console.log(checkForName);
+
+export {
+    updateUI,
+    handleSubmit
+}
