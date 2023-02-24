c3c96b103a2646bbc87ce99f59ad82a54bf2dd76
unknown <uDegr@proton.me>
2023-02-24 01:36:32 +0100

    added ui creation based on api return

--- src/client/js/uiHandler.js
+++ src/client/js/uiHandler.js
@@ -1,7 +1,30 @@
 function updateUI(response) {
-    const subjectivity = document.getElementById('subjectivity').value;
-    const scoreTag = document.getElementById('scoreTag').value;
+    console.log(response);
 
+    ;
+    let sentenceResponse = response["sentence_list"][0]
+    let scoreTag = calculateScoreTag(response.score_tag)
+    let results = document.getElementById('results');
+    results.innerHTML = `Subjectivity: ${response.subjectivity} <br>Polarity: ${scoreTag}<br> Text Snippet: ${sentenceResponse.text}`
+
+
+}
+
+function calculateScoreTag(scoreTag) {
+    switch (scoreTag) {
+        case 'P+':
+            return 'strong positive'
+        case 'P':
+            return 'positive'
+        case 'NEU':
+            return 'neutral'
+        case 'N':
+            return 'negative'
+        case 'N+':
+            return 'strong negative'
+        case 'NONE':
+            return 'without polarity'
+    }
 }
 
 export { updateUI }
